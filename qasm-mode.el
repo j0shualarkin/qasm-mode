2742f91181d68d9ca9aa76f9f2a72cdba6e9c73b
Joshua <joslarki@iu.edu>
2020-01-25 22:40:55 -0500

    add cmd to generate a template; add a font-lock class for gates

--- qasm-mode.el
+++ qasm-mode.el
@@ -6,24 +6,40 @@
 ;; Emacs Mode for QASM
 ;; QASM is Quantum Assembly Language
 
-;; working off of this link:
-;; https://www.emacswiki.org/emacs/ModeTutorial
+;; ------------------
+
+
+;; let other emacs functions know what comments are
+(defvar qasm-mode-hook
+  (add-hook 'qasm-mode-hook
+	    (lambda ()
+	      (setq-local comment-start "//")
+	      (setq-local comment-end "\n"))))
 
 ;; ------------------
 
-(defvar qasm-mode-hook nil)
-;; qasm-mode-hook allows users to run their own code when qasm-mode is run
+;; make-template : Number -> String
+;; uses an elisp function `insert` to write the string to the current buffer
+(defun make-template (n)
+    (let ((template (concat "OPENQASM 2.0;\n"
+			    "include \"qelib1.inc\";\n\n"
+			    (format "qreg in[%d];\n" n)
+			    (format "creg out[%d];\n" n))))
+      (insert template)))
 
-;; ------------------
 
 (defvar qasm-mode-map
   (let ((map (make-keymap)))
     (define-key map "\C-j" 'newline-and-indent)
+    (define-key map "\C-c\C-o"
+      (defun gen-template (n)
+	"Make a basic qasm file set up with a given amount of registers"
+	(interactive "nEnter number of registers to put in template: ")
+	(make-template n)))
     map)
   "Keymap for QASM major mode")
 
 ;; mode-map allows users to define their own keymaps
-;; set to default keymap,
 ;; the added key ("\C-j") is redundant (happens by default)
 ;; but is left as an example 
 
@@ -37,24 +53,33 @@
 
 (defconst qasm-font-lock-keywords-1
   (list
-   '("\\<\\(?:->\\|CX\\|U\\|barrier\\|creg\\|gate\\(?:name\\)?\\|if\\|measure\\|opaque\\|qreg\\|reset\\)\\>" . font-lock-builtin-face)
+   (cons (regexp-opt '("creg" "qreg" "barrier" "if"
+		       "gate" "opaque" "measure"
+		       "reset" "gatename"))
+	 font-lock-builtin-face)
    '("\\('\\w*'\\)" . font-lock-variable-name-face))
-  "Basic highlighting expressions for QASM mode")
+  "Basic expressions for QASM mode")
 
-;; strings used to generate optimized regexp above
-;; (regexp-opt '("creg" "qreg" "barrier" "if"
-;;               "include" "gate" "opaque"
-;;               "U" "CX" "measure" "reset"
-;;               "gatename" "->"))
 
 (defconst qasm-font-lock-keywords-2
   (append qasm-font-lock-keywords-1
 	  (list
-	   '("\\<\\(?:OPENQASM\\|include\\)\\>" . font-lock-keyword-face)))
-  "additional keywords to highlight in qasm-mode")
+	   (cons (regexp-opt '("OPENQASM" "include"))
+		 font-lock-keyword-face)))
+  "qasm keywords are OPENQASM and include")
+
+(defconst qasm-font-lock-keywords-3
+  (append qasm-font-lock-keywords-2
+	  (list
+	   (cons (regexp-opt '("u" "x" "cx" "ccx"
+			       "U" "x" "CX" "CCX"))
+		 font-lock-function-name-face)))
+  "have the built-in gates as functions")
+
+	    
 
 (defvar qasm-font-lock-keywords
-  qasm-font-lock-keywords-2
+  qasm-font-lock-keywords-3
   "Default syntax highlighting for qasm-mode")
 
 ;; ------------------
@@ -77,6 +102,7 @@
   (set (make-local-variable 'font-lock-defaults)
        '(qasm-font-lock-keywords))
   ;; (set (make-local-variable 'indent-line-function) 'qasm-indent-line)
+  
   (setq major-mode 'qasm-mode)
   (setq mode-name "QASM")
   (run-hooks 'qasm-mode-hook))
